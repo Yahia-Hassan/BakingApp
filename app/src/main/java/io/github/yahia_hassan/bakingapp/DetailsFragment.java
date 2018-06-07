b227f33b6348b3a34697831802870cbdabddfc3c
Yahia Hassan <yehia4747@gmail.com>
2018-06-07 02:12:03 +0200

    Add DetailsFragment

---
+++ app/src/main/java/io/github/yahia_hassan/bakingapp/DetailsFragment.java
@@ -0,0 +1,27 @@
+package io.github.yahia_hassan.bakingapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class DetailsFragment extends Fragment {
+
+
+    public DetailsFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_details, container, false);
+
+        return view;
+    }
+
+}
