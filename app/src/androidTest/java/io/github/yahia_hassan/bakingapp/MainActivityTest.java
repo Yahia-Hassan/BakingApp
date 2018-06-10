b654cc274bb26d7596833620434cfeb6752022d1
Yahia Hassan <yehia4747@gmail.com>
2018-06-10 09:40:28 +0200

    Add UI test.

--- app/src/androidTest/java/io/github/yahia_hassan/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/io/github/yahia_hassan/bakingapp/MainActivityTest.java
@@ -1,4 +1,32 @@
 package io.github.yahia_hassan.bakingapp;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.github.yahia_hassan.bakingapp.Adapters.RecipesAdapter;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityFirstItemInRecyclerViewTest() {
+        onView(withId(R.id.main_activity_recycler_view))
+                .perform(RecyclerViewActions.scrollToPosition(0)).check(matches(isDisplayed()));
+
+    }
 }
